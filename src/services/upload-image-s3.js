f338cda72a8d80437f5fcd192cdcb1d6bfcb8dc5
glauroqj <glauro.juliani@hotmail.com>
2022-08-30 22:13:09 +0200

    add

---
+++ src/services/upload-image-s3.js
@@ -0,0 +1,36 @@
+import AWS from "aws-sdk";
+
+const s3 = new AWS.S3({
+  // accessKeyId: "AKIASDIQ5NHR2LKCA774",
+  // secretAccessKey: "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",
+  // region: "sa-east-1",
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_KEY,
+  region: process.env.AWS_REGION,
+});
+
+export default ({ image, format, name }) =>
+  new Promise((resolve) => {
+    try {
+      const params = {
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: `${name}.${format}`,
+        Body: image,
+        // ACL: ''
+      };
+
+      s3.upload(params, (err, data) => {
+        if (err) {
+          throw new Error(err);
+        }
+
+        /** else */
+        resolve({
+          url: data?.key,
+          format: format,
+        });
+      });
+    } catch (e) {
+      throw new Error(e);
+    }
+  });
--- src/utils/imageMin.js
+++ src/utils/imageMin.js
@@ -1,17 +1,63 @@
 import imagemin from "imagemin";
 import imageminWEBP from "imagemin-webp"; /** WEBP */
+import imageminPngquant from "imagemin-pngquant"; /** PNG */
 
-export default (file) =>
+const uniqueID = (type) => {
+  const start = Date.now().toString(36);
+  const tail = Math.random().toString(36).substring(2);
+
+  return `image-${type || ""}-${start + tail}`;
+};
+
+const imageminHandler = (buffer, pluginsParam) =>
+  imagemin.buffer(buffer, {
+    plugins: [pluginsParam],
+  });
+
+const generateWEBP = (buffer) =>
+  new Promise(async (resolve) => {
+    try {
+      const webp = await imageminHandler(buffer, imageminWEBP({ quality: 65 }));
+
+      resolve(webp);
+    } catch (e) {
+      resolve(false);
+    }
+  });
+
+const generatePNG = (buffer) =>
+  new Promise(async (resolve) => {
+    try {
+      const png = await imageminHandler(
+        buffer,
+        imageminPngquant({ quality: [0.4, 0.6] })
+      );
+
+      resolve(png);
+    } catch (e) {
+      resolve(false);
+    }
+  });
+
+export default (buffer) =>
   new Promise((resolve, reject) => {
-    imagemin
-      .buffer(file, {
-        destination: "./uploads/images",
-        plugins: [imageminWEBP({ quality: 65 })],
+    Promise.all([generateWEBP(buffer), generatePNG(buffer)])
+      .then((values) => {
+        const [WEBP, PNG] = values;
+        resolve([
+          {
+            image: WEBP,
+            format: "webp",
+            name: uniqueID("webp"),
+          },
+          {
+            image: PNG,
+            format: "png",
+            name: uniqueID("png"),
+          },
+        ]);
       })
-      .then((response) => {
-        // upload file to S3
-        console.log("< FILES > ", response);
-        resolve(response);
-      })
-      .catch((error) => reject(error));
+      .catch((e) => {
+        reject(e);
+      });
   });
